6462ade994143edc12d5e1c547f305b1a5430dc5
Rui Xue <xuerui4app@gmail.com>
2017-04-26 08:02:29 +0800

    update q2

--- addTwoNumbers/addTwoNumbers.go
+++ addTwoNumbers/addTwoNumbers.go
@@ -6,9 +6,45 @@
  * }
  */
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-    var out *ListNode
-    out = getdigits(l1)
+    s1 := getdigits(l1)
+    s2 := getdigits(l2)
+    s := adder(s1, s2)
+    out := parser(s)
     return out
+}
+
+func parser(s []int) *ListNode {
+
+}
+
+
+
+func adder(s1 []int, s2 []int) []int {
+    n1 := toNum(s1)
+    n2 := toNum(s2)
+    n := n1 + n2
+
+    out := []int{0}
+    for n > 0 {
+        out = append(out, n%10)
+        n = n / 10
+    }
+
+    if len(out) > 1 {
+        return out[1:] 
+    } else {
+        return out
+    }
+}
+
+func toNum(s []int) int {
+    base := 1
+    var sum int
+    for _, v := range(s) {
+        sum = sum + v*base
+        base = base * 10
+    }
+    return sum
 }
 
 func getdigits(l *ListNode) []int {
